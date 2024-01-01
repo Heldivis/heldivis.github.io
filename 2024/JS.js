67d6a5dd51f4d2387dab36be49d7a28c4b7499bd
Heldivis <144578929+Heldivis@users.noreply.github.com>
2024-01-01 11:15:19 +0800

    Update JS.js
    For Lunatic New Year

--- 2024/JS.js
+++ 2024/JS.js
@@ -15,17 +15,35 @@ setInterval(function() {document.querySelector('.GetTime').innerHTML = getTime()
 
 function getTimeR() {
     var date = new Date();
-    var year = date.getFullYear();
-    if (year == 2024)
-        return "新年快乐！";
+    var month = date.getMonth() + 1; //获取月份
+    var day = date.getDate(); //获取日期
     var hour = date.getHours(); //获取小时
     var minute = date.getMinutes(); // 获取分
     var second = date.getSeconds(); //获取秒
+    if (month == 2)
+        day = day + 31;
+    var finalday = 41;
+    day = finalday - 1 - day;
+    if (day < 0)
+        return "<span style = 'font-weight: bolder;'>新年快乐！</span>";
     hour = 23 - hour;
     minute = 59 - minute;
     second = 60 - second;
+    if (second == 60) {
+        second = 0;
+        minute = minute + 1;
+    }
+    if (minute == 60) {
+        minute = 0;
+        hour = hour + 1;
+    }
+    if (hour == 24) {
+        hour = 0;
+        day = day + 1;
+    }
+    hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
     second = second < 10 ? '0' + second : second;
-    return hour + ':' + minute + ':' + second;
+    return day + "<span style = 'font-size: 0.3em;'>天</span>" + hour + ':' + minute + ':' + second;
 }
 setInterval(function() {document.querySelector('.GetTimeR').innerHTML = getTimeR();}, 1)
